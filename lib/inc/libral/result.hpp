e86523a5d5624e5c3ed2366c572c116c1175bb5e
David Lutterkort <lutter@watzmann.net>
2016-11-07 15:17:34 -0800

    Add a notion of a 'result' that mimick's Rust's Result

---
+++ lib/inc/libral/result.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+namespace libral {
+  /*
+     A poor man's emulation of Rust's Result construct. The basic idea is
+     that we want to avoid throwing exceptions and rather force our callers
+     to be explicit about how they handle errors.
+
+     One day we might be able to use std::expected but that's still
+     experimental
+  */
+
+  // The basic error we use everywhere
+  struct error {
+    error(const std::string &det) : detail(det) {};
+    std::string detail;
+  };
+
+  /* A result is either an error or whatever we really wanted */
+  template <class R>
+  class result : boost::variant<error, R> {
+    typedef boost::variant<error, R> base;
+  public:
+    result(R& ok) : base(ok) {};
+    result(const R& ok) : base(ok) {};
+    result(R&& ok) : base(ok) {};
+    result(error& err) : base(err) {};
+    result(const error& err) : base(err) {};
+    result(error&& err) : base(err) {};
+
+    boost::optional<R&> ok() {
+      if (is_ok()) {
+        return boost::get<R>(*this);
+      } else {
+        return boost::none;
+      }
+    };
+
+    boost::optional<const R&> ok() const {
+      if (is_ok()) {
+        return boost::get<R>(*this);
+      } else {
+        return boost::none;
+      }
+    };
+
+    boost::optional<error&> err() {
+      return is_err() ? boost::get<error>(*this) : boost::none;
+    };
+
+    boost::optional<const error&> err() const {
+      if (is_err()) {
+        return boost::get<error>(*this);
+      } else {
+        return boost::none;
+      }
+    };
+
+    bool is_ok()  const { return base::which() == 1; }
+    bool is_err() const { return base::which() == 0; }
+  };
+}
