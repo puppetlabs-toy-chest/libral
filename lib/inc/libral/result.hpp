52584bfbefd162e5864cecb9c40246012f7bafb3
David Lutterkort <lutter@watzmann.net>
2016-12-13 10:36:21 -0800

    Rewrite result to not use boost::variant
    Using boost::variant makes it impossible to have a result<value> because of
    https://svn.boost.org/trac/boost/ticket/7120

--- lib/inc/libral/result.hpp
+++ lib/inc/libral/result.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <boost/variant.hpp>
+#include <boost/optional.hpp>
 
 namespace libral {
   /*
@@ -10,6 +10,11 @@ namespace libral {
 
      One day we might be able to use std::expected but that's still
      experimental
+
+     We would really like to use boost::variant here, but that runs into
+     trouble along the lines of
+     https://svn.boost.org/trac/boost/ticket/7120 so we do it the
+     old-fashioned way with a lot more boilerplate code
   */
 
   // The basic error we use everywhere
@@ -20,19 +25,44 @@ namespace libral {
 
   /* A result is either an error or whatever we really wanted */
   template <class R>
-  class result : boost::variant<error, R> {
-    typedef boost::variant<error, R> base;
+  class result {
+  private:
+    enum class tag { err, ok };
   public:
-    result(R& ok) : base(ok) {};
-    result(const R& ok) : base(ok) {};
-    result(R&& ok) : base(std::move(ok)) {};
-    result(error& err) : base(err) {};
-    result(const error& err) : base(err) {};
-    result(error&& err) : base(std::move(err)) {};
+    result(R& ok) : _tag(tag::ok), _ok(ok) {};
+    result(const R& ok) : _tag(tag::ok), _ok(ok) {};
+    result(R&& ok) : _tag(tag::ok), _ok(std::move(ok)) {};
+    result(error& err) : _tag(tag::err), _err(err) {};
+    result(const error& err) : _tag(tag::err), _err(err) {};
+    result(error&& err) : _tag(tag::err), _err(std::move(err)) {};
+    result(const result& other) {
+      if (other.is_ok()) {
+        _ok = other._ok;
+      } else {
+        _err = other._err;
+      }
+    }
+
+    result& operator=(const result& other) {
+      if (other.is_ok()) {
+        _ok = other._ok;
+      } else {
+        _err = other._err;
+      }
+      return *this;
+    }
+
+    ~result() {
+      if (is_ok()) {
+        _ok.~R();
+      } else {
+        _err.~error();
+      }
+    }
 
     boost::optional<R&> ok() {
       if (is_ok()) {
-        return boost::get<R>(*this);
+        return this->_ok;
       } else {
         return boost::none;
       }
@@ -40,7 +70,7 @@ namespace libral {
 
     boost::optional<const R&> ok() const {
       if (is_ok()) {
-        return boost::get<R>(*this);
+        return this->_ok;
       } else {
         return boost::none;
       }
@@ -48,7 +78,7 @@ namespace libral {
 
     boost::optional<error&> err() {
       if (is_err()) {
-        return boost::get<error>(*this);
+        return this->_err;
       } else {
         return boost::none;
       }
@@ -56,14 +86,14 @@ namespace libral {
 
     boost::optional<const error&> err() const {
       if (is_err()) {
-        return boost::get<error>(*this);
+        return this->_err;
       } else {
         return boost::none;
       }
     };
 
-    bool is_ok()  const { return base::which() == 1; }
-    bool is_err() const { return base::which() == 0; }
+    bool is_ok()  const { return _tag == tag::ok; }
+    bool is_err() const { return _tag == tag::err; }
 
     static std::unique_ptr<result<R>> make_unique() {
       return std::unique_ptr<result<R>>(new result<R>(R()));
@@ -71,6 +101,12 @@ namespace libral {
 
     static std::unique_ptr<result<R>> make_unique(const error& e) {
       return std::unique_ptr<result<R>>(new result<R>(e));
+    };
+  private:
+    tag _tag;
+    union {
+      error _err;
+      R     _ok;
     };
   };
 }
