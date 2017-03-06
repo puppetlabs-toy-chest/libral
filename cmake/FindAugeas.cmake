a298aa698a2aaa6018657b930532c87d1d298fd1
David Lutterkort <lutter@watzmann.net>
2017-03-05 17:31:40 -0800

    cmake/FindAugeas.cmake: avoid pointless static link against libm

--- cmake/FindAugeas.cmake
+++ cmake/FindAugeas.cmake
@@ -3,7 +3,10 @@
 # Distributed under the BSD license. See COPYING-CMAKE-SCRIPTS for details.
 
 find_package(PkgConfig QUIET REQUIRED)
-pkg_check_modules(AUGEAS "augeas")
+if (AUGEAS_STATIC)
+  message("If CMake fails to find Augeas when you are using pl-build-tools, run cmake once more. Seems to be a bug in the pl-cmake setup")
+endif (AUGEAS_STATIC)
+pkg_check_modules(AUGEAS "augeas" REQUIRED)
 set (AUGEAS_DEFINITIONS ${PC_AUGEAS_CFLAGS_OTHER})
 
 find_path (AUGEAS_INCLUDE_DIR augeas.h
@@ -14,8 +17,11 @@ if (AUGEAS_STATIC)
     HINTS ${PC_AUGEAS_LIBDIR} ${PC_AUGEAS_LIBRARY_DIRS} )
   # This works, but seems pretty kludgy. One of the problems we need to
   # work around is that upstream's augeas.pc is busted and is missing some
-  # libraries from the linker flags
-  set(AUGEAS_LIBRARIES "-Wl,-Bstatic ${AUGEAS_STATIC_LDFLAGS} -lfa -lselinux -Wl,-Bdynamic")
+  # libraries from the linker flags. The other is that pkg-config wants to
+  # statically link against libm, which is pointless as that is part of
+  # glibc, and we still link to glibc dynamically even in static builds
+  list(REMOVE_ITEM AUGEAS_STATIC_LDFLAGS "-lm")
+  set(AUGEAS_LIBRARIES "-Wl,-Bstatic;${AUGEAS_STATIC_LDFLAGS};-lfa;-lselinux;-Wl,-Bdynamic")
 else (AUGEAS_STATIC)
   find_library (AUGEAS_LIBRARY NAMES augeas
     HINTS ${PC_AUGEAS_LIBDIR} ${PC_AUGEAS_LIBRARY_DIRS} )
