d79f8b897321522f17404c32b8a0b97535f9e69f
David Lutterkort <lutter@watzmann.net>
2017-04-05 15:27:35 -0700

    Add convenience wrappers for provider set/get to type

--- lib/src/type.cc
+++ lib/src/type.cc
@@ -12,12 +12,17 @@ using namespace leatherman::locale;
 
 namespace libral {
 
-  result<boost::optional<resource>>
-  type::find(const std::string &name) {
-    std::vector<std::string> names = { name };
+  result<std::vector<resource>>
+  type::get(const std::vector<std::string>& names) {
     resource::attributes config;
     context ctx { _prov };
-    auto rsrcs = _prov->get(ctx, names, config);
+
+    return _prov->get(ctx, names, config);
+  }
+
+  result<boost::optional<resource>>
+  type::find(const std::string &name) {
+    auto rsrcs = get({ name });
     err_ret(rsrcs);
 
     boost::optional<resource> res;
@@ -35,21 +40,13 @@ namespace libral {
   }
 
   result<std::vector<resource>> type::instances(void) {
-    std::vector<std::string> names;
-    resource::attributes config;
-    context ctx { _prov };
-    auto result = _prov->get(ctx, names, config);
-
-    return result;
+    return get({ });
   }
 
   result<std::pair<update, changes>>
   type::set(const resource& should) {
     auto opt_rsrc = find(should.name());
-
-    if (!opt_rsrc) {
-      return opt_rsrc.err();
-    }
+    err_ret(opt_rsrc);
 
     resource is = (*opt_rsrc) ? std::move(**opt_rsrc)
       : _prov->create(should.name());
@@ -58,13 +55,51 @@ namespace libral {
     update upd = { .is = is, .should = should };
 
     auto res = _prov->set(ctx, { upd });
-    if (!res) {
-      return res.err();
-    }
+    err_ret(res);
 
     auto chgs = ctx.changes_for(should.name());
 
     return std::pair<update, changes>(std::move(upd), std::move(chgs));
+  }
+
+  result<std::vector<std::pair<update, changes>>>
+  type::set(const std::vector<resource>& shoulds) {
+    context ctx { _prov };
+    resource::attributes config;
+
+    // get the resoures mentioned in should
+    std::vector<std::string> names;
+
+    for (const auto& should : shoulds) {
+      names.push_back(should.name());
+    }
+    auto r = _prov->get(ctx, names, config);
+    err_ret(r);
+
+    auto iss = r.ok();
+
+    updates upds;
+    for (const auto& should: shoulds) {
+      bool found = false;
+      for (const auto& is : iss) {
+        if (is.name() == should.name()) {
+          found = true;
+          upds.push_back({ .is = is, .should = should });
+        }
+      }
+      if (!found) {
+        auto is = _prov->create(should.name());
+        upds.push_back({ .is = is, .should = should });
+      }
+    }
+
+    err_ret( _prov->set(ctx, upds) );
+
+    std::vector<std::pair<update, changes>> res;
+    for (const auto& upd : upds) {
+      res.push_back(std::make_pair(upd, ctx.changes_for(upd.name())));
+    }
+    return res;
   }
 
   result<value> type::parse(const std::string &name, const std::string &v) {
