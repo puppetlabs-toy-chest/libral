55e17d95770e328e7b897149d53fa0919a2c0b17
David Lutterkort <lutter@watzmann.net>
2017-04-21 12:12:50 -0700

    Do not statically initialize the ral or the file provider
    Trying to do this initialization statically leads to segfaults as the
    logging subsystem isn't fully initialized when this happens. The segfault
    happens when ral tries to report an error about a nonsuitable provider in
    leatherman::locale::get_locale

--- lib/tests/file.cc
+++ lib/tests/file.cc
@@ -12,150 +12,158 @@
 namespace fs = boost::filesystem;
 
 namespace libral {
-  auto aral = ral::create({ });
+  std::unique_ptr<type> file_type;
 
-  auto type = *aral->find_type("file::posix");
-  provider& prv = type->prov();
+  SCENARIO("file::posix provider") {
+    auto aral = ral::create({ });
 
-  resource::attributes config;
+    // Cache the file_type so we do not have to go find it on every test
+    // run
+    if (!file_type) {
+      file_type = *aral->find_type("file::posix");
+    }
+    provider& prv = file_type->prov();
 
-  SCENARIO("instances() returns an empty vector") {
-    libral::context ctx { type->prov_ptr() };
+    resource::attributes config;
 
-    REQUIRE(! prv.get(ctx, {}, config));
-  }
+    SECTION("instances() returns an empty vector") {
+      libral::context ctx { file_type->prov_ptr() };
 
-  SCENARIO("suitable() is always true") {
-    auto spec = prv.spec();
-    REQUIRE(spec);
-    REQUIRE(spec->suitable());
-  }
-
-  SCENARIO("find()") {
-    libral::context ctx { type->prov_ptr() };
-
-    SECTION("returns resource for nonexistent file") {
-      auto res = prv.get(ctx, { "/tmp/not_there" }, config);
-
-      REQUIRE(res.is_ok());
-      REQUIRE(res.ok().size() == 1);
-      auto& rsc = res.ok().front();
-      REQUIRE(rsc["ensure"] == value("absent"));
+      REQUIRE(! prv.get(ctx, {}, config));
     }
 
-    SECTION("finds an existing file") {
-      auto tmp = temp_file("");
-
-      auto res = prv.get(ctx, { tmp.get_file_name() }, config);
-
-      REQUIRE(res.is_ok());
-      REQUIRE(res.ok().size() == 1);
-      auto& rsc = res.ok().front();
-      REQUIRE(rsc["ensure"] == value("file"));
-      auto full_name = fs::canonical(tmp.get_file_name());
-      REQUIRE(rsc.name() == full_name);
+    SECTION("suitable() is always true") {
+      auto spec = prv.spec();
+      REQUIRE(spec);
+      REQUIRE(spec->suitable());
     }
-  }
 
-  SCENARIO("update()") {
-    libral::context ctx { type->prov_ptr() };
+    SECTION("find()") {
+      libral::context ctx { file_type->prov_ptr() };
 
-    SECTION("create a new file") {
-      auto tmp = unique_fixture_path();
-      try {
-        auto res = prv.get(ctx, { tmp.native() }, config);
+      SECTION("returns resource for nonexistent file") {
+        auto res = prv.get(ctx, { "/tmp/not_there" }, config);
+
         REQUIRE(res.is_ok());
         REQUIRE(res.ok().size() == 1);
         auto& rsc = res.ok().front();
         REQUIRE(rsc["ensure"] == value("absent"));
+      }
 
-        resource should = prv.create(tmp.native());
-        should["ensure"] = "present";
-        should["mode"] = "0660";
-        should["content"] = "stuff";
+      SECTION("finds an existing file") {
+        auto tmp = temp_file("");
 
-        {
+        auto res = prv.get(ctx, { tmp.get_file_name() }, config);
+
+        REQUIRE(res.is_ok());
+        REQUIRE(res.ok().size() == 1);
+        auto& rsc = res.ok().front();
+        REQUIRE(rsc["ensure"] == value("file"));
+        auto full_name = fs::canonical(tmp.get_file_name());
+        REQUIRE(rsc.name() == full_name);
+      }
+    }
+
+    SECTION("update()") {
+      libral::context ctx { file_type->prov_ptr() };
+
+      SECTION("create a new file") {
+        auto tmp = unique_fixture_path();
+        try {
+          auto res = prv.get(ctx, { tmp.native() }, config);
+          REQUIRE(res.is_ok());
+          REQUIRE(res.ok().size() == 1);
+          auto& rsc = res.ok().front();
+          REQUIRE(rsc["ensure"] == value("absent"));
+
+          resource should = prv.create(tmp.native());
+          should["ensure"] = "present";
+          should["mode"] = "0660";
+          should["content"] = "stuff";
+
+          {
+            updates upds = { { .is = rsc, .should = should } };
+            auto res = prv.set(ctx, upds);
+            REQUIRE(res.is_ok());
+            auto& chgs = ctx.changes_for(rsc.name());
+            REQUIRE(chgs.exists("ensure"));
+            REQUIRE(chgs.exists("mode"));
+
+            auto changed = prv.create(upds.front(), chgs);
+            REQUIRE(changed["ensure"] == value("file"));
+            REQUIRE(changed["mode"] == value("0660"));
+            REQUIRE(changed["content"] == value("stuff"));
+          }
+          fs::remove(tmp);
+        } catch(...) {
+          fs::remove(tmp);
+          throw;
+        }
+      }
+
+      SECTION("mutates file type") {
+        // Here are all the possible file type changes; we should totally
+        // test them:
+        //     AA AF FF FD DF FA AD DD DL LD DA AL LF FL LL LA
+        // Right now, we have tests for AF FL LD
+        auto tmp = temp_file("original");
+        auto res = prv.get(ctx, { tmp.get_file_name() }, config);
+        REQUIRE(res.is_ok());
+        REQUIRE(res.ok().size() == 1);
+        auto& rsc = res.ok().front();
+
+        SECTION("file -> link") {
+          auto should = prv.create(rsc.name());
+          should["ensure"] = "link";
+          should["target"] = "/tmp";
+
+          updates upds = { { .is = rsc, .should = should } };
+          auto res = prv.set(ctx, upds);
+          if (!res) {
+            std::cout << res.err().detail << std::endl;
+          }
+          REQUIRE(res.is_ok());
+          auto& chgs = ctx.changes_for(rsc.name());
+          REQUIRE(chgs.size() == 2);
+          REQUIRE(chgs.exists("ensure"));
+          REQUIRE(chgs.exists("target"));
+
+          auto changed = prv.create(upds.front(), chgs);
+          REQUIRE(changed["ensure"] == value("link"));
+          REQUIRE(changed["target"] == value("/tmp"));
+        }
+
+        SECTION("change link target") {
+          auto should = prv.create(rsc.name());
+          should["ensure"] = "link";
+          should["target"] = "/var/tmp";
+
           updates upds = { { .is = rsc, .should = should } };
           auto res = prv.set(ctx, upds);
           REQUIRE(res.is_ok());
           auto& chgs = ctx.changes_for(rsc.name());
-          REQUIRE(chgs.exists("ensure"));
-          REQUIRE(chgs.exists("mode"));
+          REQUIRE(chgs.size() == 2);
+          REQUIRE(chgs.exists("target"));
 
           auto changed = prv.create(upds.front(), chgs);
-          REQUIRE(changed["ensure"] == value("file"));
-          REQUIRE(changed["mode"] == value("0660"));
-          REQUIRE(changed["content"] == value("stuff"));
+          REQUIRE(changed["ensure"] == value("link"));
+          REQUIRE(changed["target"] == value("/var/tmp"));
         }
-        fs::remove(tmp);
-      } catch(...) {
-          fs::remove(tmp);
-          throw;
-      }
-    }
 
-    SECTION("mutates file type") {
-      // Here are all the possible file type changes; we should totally
-      // test them:
-      //     AA AF FF FD DF FA AD DD DL LD DA AL LF FL LL LA
-      // Right now, we have tests for AF FL LD
-      auto tmp = temp_file("original");
-      auto res = prv.get(ctx, { tmp.get_file_name() }, config);
-      REQUIRE(res.is_ok());
-      REQUIRE(res.ok().size() == 1);
-      auto& rsc = res.ok().front();
+        SECTION("link -> directory") {
+          auto should = prv.create(rsc.name());
+          should["ensure"] = "directory";
 
-      SECTION("file -> link") {
-        auto should = prv.create(rsc.name());
-        should["ensure"] = "link";
-        should["target"] = "/tmp";
+          updates upds = { { .is = rsc, .should = should } };
+          auto res = prv.set(ctx, upds);
+          REQUIRE(res.is_ok());
+          auto& chgs = ctx.changes_for(rsc.name());
+          REQUIRE(chgs.size() == 1);
+          REQUIRE(chgs.exists("ensure"));
 
-        updates upds = { { .is = rsc, .should = should } };
-        auto res = prv.set(ctx, upds);
-        if (!res) {
-          std::cout << res.err().detail << std::endl;
+          auto changed = prv.create(upds.front(), chgs);
+          REQUIRE(changed["ensure"] == value("directory"));
         }
-        REQUIRE(res.is_ok());
-        auto& chgs = ctx.changes_for(rsc.name());
-        REQUIRE(chgs.size() == 2);
-        REQUIRE(chgs.exists("ensure"));
-        REQUIRE(chgs.exists("target"));
-
-        auto changed = prv.create(upds.front(), chgs);
-        REQUIRE(changed["ensure"] == value("link"));
-        REQUIRE(changed["target"] == value("/tmp"));
-      }
-
-      SECTION("change link target") {
-        auto should = prv.create(rsc.name());
-        should["ensure"] = "link";
-        should["target"] = "/var/tmp";
-
-        updates upds = { { .is = rsc, .should = should } };
-        auto res = prv.set(ctx, upds);
-        REQUIRE(res.is_ok());
-        auto& chgs = ctx.changes_for(rsc.name());
-        REQUIRE(chgs.size() == 2);
-        REQUIRE(chgs.exists("target"));
-
-        auto changed = prv.create(upds.front(), chgs);
-        REQUIRE(changed["ensure"] == value("link"));
-        REQUIRE(changed["target"] == value("/var/tmp"));
-      }
-
-      SECTION("link -> directory") {
-        auto should = prv.create(rsc.name());
-        should["ensure"] = "directory";
-
-        updates upds = { { .is = rsc, .should = should } };
-        auto res = prv.set(ctx, upds);
-        REQUIRE(res.is_ok());
-        auto& chgs = ctx.changes_for(rsc.name());
-        REQUIRE(chgs.size() == 1);
-        REQUIRE(chgs.exists("ensure"));
-
-        auto changed = prv.create(upds.front(), chgs);
-        REQUIRE(changed["ensure"] == value("directory"));
       }
     }
   }
--- lib/tests/main.cc
+++ lib/tests/main.cc
@@ -1,8 +1,16 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+#include <boost/nowide/iostream.hpp>
+
+#include <leatherman/logging/logging.hpp>
+
+using namespace leatherman::logging;
+
 int main(int argc, char **argv)
 {
-    return Catch::Session().run(argc, argv);
-}
+  setup_logging(boost::nowide::cerr);
+  set_level(log_level::none);
 
+  return Catch::Session().run(argc, argv);
+}
