097ef83ccc2833268cc8a3d26932c735502e8370
David Lutterkort <lutter@watzmann.net>
2017-06-15 15:39:06 -0700

    Have tests look for executables (mruby) in the build directory first

--- lib/tests/main.cc
+++ lib/tests/main.cc
@@ -4,13 +4,22 @@
 #include <boost/nowide/iostream.hpp>
 
 #include <leatherman/logging/logging.hpp>
+#include <leatherman/util/environment.hpp>
+
+#include "fixtures.hpp"
 
 using namespace leatherman::logging;
+namespace util = leatherman::util;
 
 int main(int argc, char **argv)
 {
   setup_logging(boost::nowide::cerr);
   set_level(log_level::none);
+
+  // Modify PATH so that our mruby is on it
+  std::string path;
+  util::environment::get("PATH", path);
+  util::environment::set("PATH", TEST_BIN_DIR ":" + path);
 
   return Catch::Session().run(argc, argv);
 }
